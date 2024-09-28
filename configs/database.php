1eb560e0dc95e556e58d23ad9359c4cbfe43d9ef
DANGANHSON03 <anhson22062003xxx@gmail.com>
2024-09-28 20:51:42 +0700

    first commit

--- configs/database.php
+++ configs/database.php
@@ -1,7 +1,7 @@
 <?php
 $host = "mysql:host=localhost:3306;dbname=v_store";
 $username = "root";
-$pass = "mai";
+$pass = "maic";
 try {
     $conn = new PDO($host, $username, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
